4842e74edfc658a542a3a5d97a317a834bc7f15a
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2019-07-02 01:57:58 +0900

    途中16529も見ようとしている

--- uecs-trx.py
+++ uecs-trx.py
@@ -21,8 +21,8 @@ XML_HEADER  = "<?xml version=\"1.0\"?>"
 UECS_HEADER = "<UECS ver=\"1.00-E10\">"
 HOST = netifaces.ifaddresses('enp3s0')[netifaces.AF_INET][0]['addr']
 ADDRESS = netifaces.ifaddresses('enp3s0')[netifaces.AF_INET][0]['broadcast']
-PORT = 16529
-PORT = 16520
+CPORT = 16529
+DPORT = 16520
 #config = configparser.ConfigParser()
 #config.read('/etc/uecs/config.ini')
 
@@ -34,7 +34,7 @@ def btn_quit():
 
 rxp = tk.Tk()
 rxp.title("受信機")
-rxp.geometry("1280x770")
+rxp.geometry("1280x600")
 mainFrame = tk.Frame(rxp)
 rxmenu = tk.Menu(rxp)
 rxp.configure(menu=rxmenu)
@@ -43,17 +43,30 @@ rxmenu_file   = tk.Menu(rxmenu,tearoff=False)
 rxmenu_format = tk.Menu(rxmenu,tearoff=False)
 rxmenu_help   = tk.Menu(rxmenu,tearoff=False)
 
-rxmenu.add_cascade(label='File',   underline=0, menu=rxmenu_file)
-rxmenu.add_cascade(label='Format', underline=0, menu=rxmenu_format)
-rxmenu.add_cascade(label='Help',   underline=0, menu=rxmenu_help)
+rxmenu.add_cascade(label='ファイル', underline=0, menu=rxmenu_file)
+rxmenu.add_cascade(label='出力書式', underline=0, menu=rxmenu_format)
+rxmenu.add_cascade(label='ヘルプ',   underline=0, menu=rxmenu_help)
 #
-rxmenu_file.add_command(label='ABOUT',command=menu_about)
+rxmenu_file.add_command(label='このプログラムについて',command=menu_about)
 rxmenu_file.add_separator()
-rxmenu_file.add_command(label='QUIT',command=btn_quit)
+rxmenu_file.add_command(label='終了',command=btn_quit)
 #
-addtod = tk.IntVar()
-rxmenu_format.add_checkbutton(label='ADD TOD',onvalue=1, offvalue=0, variable=addtod)
-rxmenu_help.add_command(label='ABOUT',command=menu_about)
+addtod = tk.BooleanVar()
+shorttext = tk.BooleanVar()
+shortesttext = tk.BooleanVar()
+hidencnd = tk.BooleanVar()
+addtod.set(False)
+shorttext.set(False)
+shortesttext.set(False)
+hidencnd.set(False)
+
+rxmenu_format.add_checkbutton(label='日付を追加する',variable=addtod)
+rxmenu_format.add_checkbutton(label='短縮表示',variable=shorttext)
+rxmenu_format.add_checkbutton(label='極超短縮表示',variable=shortesttext)
+rxmenu_format.add_checkbutton(label='cndを表示しない',variable=hidencnd)
+#
+
+rxmenu_help.add_command(label='このプログラムについて',command=menu_about)
 mainFrame.grid()
 
 titleFrame = tk.Frame(mainFrame)
@@ -68,36 +81,39 @@ dpd.set(True)
 cpd = tk.BooleanVar()
 cpd.set(True)
 
-dport = tk.Checkbutton(optionFrame,variable=dpd,text="16520")
-cport = tk.Checkbutton(optionFrame,variable=cpd,text="16529")
+cb_dport = tk.Checkbutton(optionFrame,variable=dpd,text="16520")
+cb_cport = tk.Checkbutton(optionFrame,variable=cpd,text="16529")
 
 optlbl1.pack(side=tk.LEFT)
-dport.pack()
-cport.pack()
+cb_dport.pack()
+cb_cport.pack()
 optionFrame.grid(column=0,row=1)
 
-textFrame = tk.Frame(mainFrame,width=1270,height=250)
+textFrame = tk.Frame(mainFrame,width=1270,height=200)
 textFrame.grid()
-rtext = tk.Text(textFrame,bg='white',width=180,height=30,relief="groove")
+rtext = tk.Text(textFrame,bg='white',width=180,height=20,relief="groove")
 rtext.pack(side=tk.LEFT,padx=(3,0),pady=(3,3))
 scrollbar = tk.Scrollbar(textFrame,orient=tk.VERTICAL,command=rtext.yview)
 scrollbar.pack(side=tk.RIGHT,fill="y")
 rtext["yscrollcommand"] = scrollbar.set
 
-class ServerThread(threading.Thread):
-    def __init__(self, PORT):
+class DataUDP(threading.Thread):
+    def __init__(self):
         threading.Thread.__init__(self)
         self.data = 'hoge'
         self.kill_flag = False
         # line information
-        self.HOST = ""
-        self.PORT = PORT
+        self.HOST = "192.168.38.255"
+        self.DPORT = DPORT
         self.BUFSIZE = 512
-        self.ADDR = (gethostbyname(self.HOST), self.PORT)
+        #self.DADDR = (gethostbyname(self.HOST), self.DPORT)
+        self.DADDR = (self.HOST,self.DPORT)
+        print("HOST={0}".format(gethostbyname(self.HOST)))
         # bind
-        self.udpServSock = socket(AF_INET, SOCK_DGRAM)
-        self.udpServSock.setsockopt(SOL_SOCKET,SO_REUSEADDR|SO_BROADCAST,1)
-        self.udpServSock.bind(self.ADDR) # HOST, PORTでbinding
+        # DATA PORT
+        self.udpDSock = socket(AF_INET, SOCK_DGRAM)
+        self.udpDSock.setsockopt(SOL_SOCKET,SO_REUSEADDR|SO_BROADCAST,1)
+        self.udpDSock.bind(self.DADDR) # HOST, PORTでbinding
         # Get Network information by myself
         self.ipaddress = netifaces.ifaddresses('enp3s0')[netifaces.AF_INET][0]['addr']
         self.node = uuid.getnode()
@@ -105,88 +121,123 @@ class ServerThread(threading.Thread):
         self.macaddr = self.mac.hex[-12:].upper()
 
     def run(self):
+        v = {}
         while True:
             a=datetime.datetime.now()
-            s="{0:4d}/{1:02d}/{2:02d} {3:2d}:{4:02d}:{5:02d}"\
+            s="{0:4d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}"\
                 .format(a.year,a.month,a.day,a.hour,a.minute,a.second)
 
             # try:
-            data, self.addr = self.udpServSock.recvfrom(self.BUFSIZE) # データ受信
-            self.data = data.decode('utf-8').rstrip("\n")
-            if (addtod.get()==0):
-                txt = "{0}".format(self.data)
-            else:
-                txt = "{0} {1}".format(s,self.data)
-            rtext.insert('end',txt+'\n')
-            rtext.see('end')
-            print(txt)
-            print("cport={0}  dport={1}".format(cpd.get(),dpd.get()))
-            root = ET.fromstring(self.data)
+            ddata, self.daddr = self.udpDSock.recvfrom(self.BUFSIZE) # データ受信
+            dtext = ddata.decode('utf-8')
+            self.ddata = dtext.rstrip("\n")
+            root = ET.fromstring(self.ddata)
             for c1 in root:
                 sp = c1.tag
-                print("SPd=:{0}:".format(sp))
+                v[sp] = c1.text
+                #print("{0}={1}".format(sp,v[sp]))
                 for c2 in c1.attrib:
-                    print(c2)
+                    v[c2] = c1.attrib[c2]
+                    #print("c2[{0}]={1}".format(c2,v[c2]))
+            shtype = v['type'][0:3]
+            if (hidencnd.get() and (shtype=='cnd')):
+                pass
+            else:
+                if (not shortesttext.get()):
+                    if (shorttext.get()):
+                        dtext = dtext.replace(XML_HEADER+UECS_HEADER,"")
+                        dtext = dtext.replace("</UECS>","")
+                else:
+                    dtext = "{0},{1},{2},{3},{4},{5},{6}"\
+                            .format(v['type'],v['room'],v['region'],v['order'],\
+                                    v['priority'],v['DATA'],v['IP'])
+                
+                if (addtod.get()):
+                    txt = "{0} {1}".format(s,dtext)
+                else:
+                    txt = "{0}".format(dtext)
+                rtext.insert('end',txt+'\n')
+                rtext.see('end')
+                
+            print(txt)
+            print("dport={0}".format(dpd.get()))
 
-#                 if ( sp == 'NODESCAN' ):
-#                     self.sdata = "{0}{1}<NODE><NAME>{2}</NAME><VENDER>{3}</VENDER>"\
-#                                  "<UECSID>{4}</UECSID><IP>{5}</IP><MAC>{6}</MAC></UECS>".\
-#                                  format(XML_HEADER,UECS_HEADER,config['NODE']['name'],config['NODE']['vender'],\
-#                                         config['NODE']['uecsid'],self.ipaddress,self.macaddr)
-#                     self.udpServSock.sendto(self.sdata.encode('utf-8'),self.addr)
+class CtrlUDP(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.data = 'hoge'
+        self.kill_flag = False
+        # line information
+        self.HOST = ""
+        self.CPORT = CPORT
+        self.BUFSIZE = 512
+        self.CADDR = (gethostbyname(self.HOST), self.CPORT)
+        # bind
+        # CONTROL PORT
+        self.udpCSock = socket(AF_INET, SOCK_DGRAM)
+        self.udpCSock.setsockopt(SOL_SOCKET,SO_REUSEADDR|SO_BROADCAST,1)
+        self.udpCSock.bind(self.CADDR) # HOST, PORTでbinding
+        # Get Network information by myself
+        self.ipaddress = netifaces.ifaddresses('enp3s0')[netifaces.AF_INET][0]['addr']
+        self.node = uuid.getnode()
+        self.mac = uuid.UUID(int=self.node)
+        self.macaddr = self.mac.hex[-12:].upper()
 
-#                 # CCMSCAN
-#                 elif ( sp == 'CCMSCAN' ):
-#                     ccm = ET.parse(config['NODE']['xmlfile'])
-#                     ccmroot = ccm.getroot()
-#                     maxx    = len(ccmroot)
-#                     maxy    = int((maxx+1)/2)
-#                     cpag    = int(c1.attrib['page'])
-#                     curx    = int(cpag-1)*2
-#                     ccmt    = ccmroot[curx]
-#                     ccmnum  = curx+1
-#                     if (ccmnum < maxx):
-#                         ccmcount = 2
-#                     else:
-#                         ccmcount = 1
-#                     ccmtt   = config[ccmt.text]
-#                     ccmdata = "{0}{1}<CCMNUM page=\"{2}\" total=\"{3}\">{4}</CCMNUM>"\
-#                               "<CCM No=\"{5}\" room=\"{6}\" region=\"{7}\" order=\"{8}\" "\
-#                               "priority=\"{9}\" cast=\"{10}\" unit=\"{11}\" SR=\"{12}\" "\
-#                               "LV=\"{13}\">{14}</CCM>"\
-#                               .format(XML_HEADER,UECS_HEADER,cpag,maxy,ccmcount,
-#                                       ccmnum,ccmtt['room'],ccmtt['region'],ccmtt['order'],
-#                                       ccmtt['priority'],ccmt.attrib['cast'],ccmt.attrib['unit'],ccmt.attrib['SR'],
-#                                       ccmt.attrib['LV'],ccmt.text)
-#                     curx   += 1
-#                     ccmnum  = curx+1
-#                     if (curx < maxx):
-#                         if (ccmroot[curx]!=""):
-#                             ccmt  = ccmroot[curx]
-#                             ccmtt = config[ccmt.text]
-#                             ccmdata += "<CCM No=\"{0}\" room=\"{1}\" region=\"{2}\" order=\"{3}\" "\
-#                                        "priority=\"{4}\" cast=\"{5}\" unit=\"{6}\" SR=\"{7}\" "\
-#                                        "LV=\"{8}\">{9}</CCM>"\
-#                                        .format(ccmnum,ccmtt['room'],ccmtt['region'],ccmtt['order'],
-#                                                ccmtt['priority'],ccmt.attrib['cast'],ccmt.attrib['unit'],ccmt.attrib['SR'],
-#                                                ccmt.attrib['LV'],ccmt.text)
-#                     ccmdata += "</UECS>"
-#                     self.udpServSock.sendto(ccmdata.encode('utf-8'),self.addr)
-#                 else:
-#                     pass
-# #            except:
-# #                pass
+    def run(self):
+        v = {}
+        while True:
+            a=datetime.datetime.now()
+            s="{0:4d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}"\
+                .format(a.year,a.month,a.day,a.hour,a.minute,a.second)
+
+            # try:
+            ddata, self.daddr = self.udpCSock.recvfrom(self.BUFSIZE) # データ受信
+            dtext = ddata.decode('utf-8')
+            self.ddata = dtext.rstrip("\n")
+            # root = ET.fromstring(self.ddata)
+            # for c1 in root:
+            #     sp = c1.tag
+            #     v[sp] = c1.text
+            #     #print("{0}={1}".format(sp,v[sp]))
+            #     for c2 in c1.attrib:
+            #         v[c2] = c1.attrib[c2]
+            #         #print("c2[{0}]={1}".format(c2,v[c2]))
+            # shtype = v['type'][0:3]
+            # if (hidencnd.get() and (shtype=='cnd')):
+            #     pass
+            # else:
+            if (True):
+                if (not shortesttext.get()):
+                    if (shorttext.get()):
+                        dtext = dtext.replace(XML_HEADER+UECS_HEADER,"")
+                        dtext = dtext.replace("</UECS>","")
+                else:
+                    dtext = "{0},{1},{2},{3},{4},{5},{6}"\
+                            .format(v['type'],v['room'],v['region'],v['order'],\
+                                    v['priority'],v['DATA'],v['IP'])
+                
+                if (addtod.get()):
+                    txt = "{0} {1}".format(s,dtext)
+                else:
+                    txt = "{0}".format(dtext)
+                rtext.insert('end',txt+'\n')
+                rtext.see('end')
+                
+            print(txt)
+            print("cport={0}  dport={1}".format(cpd.get(),dpd.get()))
 
 
 if __name__ == '__main__':
-    th = ServerThread(PORT)
-    th.setDaemon(True)
-    th.start()
-    itv = 0     # Interval Counter
+    dtudp = DataUDP()
+    dtudp.setDaemon(True)
+    dtudp.start()
+    ctudp = CtrlUDP()
+    ctudp.setDaemon(True)
+    ctudp.start()
     rxp.mainloop()
 
     while True:
-        if not th.data:
+        if not dtudp.data:
             pass
         # print(th.data)
        # time.sleep(0.1)
